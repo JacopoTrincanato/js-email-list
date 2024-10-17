40c9ec84a0834cd1df8849445632c6d9dea8889c
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-17 15:54:15 +0200

    create generateEmails function and for cicle

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 //Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.  
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(response =>{
-    console.log(response);
-      
-});
-
 //richiamo l'elemento della DOM in cui voglio inserire le mail e lo salvo in una costante
-const mailLiEl = document.querySelector('li');
+const mailContainerEl = document.querySelector('ul');
+
+//creo una funzione che permette di generare una mail
+function generateEmails(email) {
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(response =>{
+    console.log(response);
+    
+    let emailEl =  response.data.response //document.createElement('li');
+    console.log(emailEl);
+    
+    return email
+  
+
+});
+    
+}
+
+//creo un ciclo che mi permetta di generare dieci mail
+for (let i = 1; i <= 10; i++) {
+    generateEmails()
+    
+}
+
+
+
+
+
+
+
+
+
+
+
